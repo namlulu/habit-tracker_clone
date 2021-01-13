b5ce81c8a62e1e903d31f5b7b75d4c8f4065dd93
sean <tmddnr3163@gmail.com>
2021-01-14 01:26:39 +0900

    2.0 Habit tracker

---
+++ src/components/habits.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import HabbitAddForm from './habbitAddForm';
+import Habit from './habit';
+
+class Habits extends Component {
+  render() {
+    return (
+      <>
+        <div className="habits">
+          <HabbitAddForm onAdd={this.props.onAdd} />
+          <ul>
+            {this.props.habits.map((habit) => (
+              <Habit
+                key={habit.id}
+                habit={habit}
+                onIncrement={this.props.onIncrement}
+                onDecrement={this.props.onDecrement}
+                onDelete={this.props.onDelete}
+              />
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Habits;
